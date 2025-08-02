eab43ef33838e816d79f75e524b6e0598f043a70
betingrich4 <mariseltru@gmail.com>
2025-08-02 11:34:36 +0300

    Update marisel.js

--- marisel.js
+++ marisel.js
@@ -68,7 +68,7 @@ setInterval(clearTempDir, 5 * 60 * 1000);
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.replace("Mercedes~", '');
+const sessdata = config.SESSION_ID.replace("trashcore~", '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
